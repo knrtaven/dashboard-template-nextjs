3d8da58a32978c5980f37be8ab6f58536b0fee14
knrtaven <52444839+knrtaven@users.noreply.github.com>
2025-07-03 12:42:59 +0800

    temproary reroute to signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-
+'use client'
 import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation' // or 'next/router' for Pages Router
 
-const page = () => {
-
+const Page = () => {
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push('/signin')
+  }, [])
   return (
-    <div>page</div>
+    <div>Redirecting to sign in..</div>
   )
 }
 
-export default page
+export default Page
