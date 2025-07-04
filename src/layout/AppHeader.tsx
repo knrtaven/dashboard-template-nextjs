15813e25de5228617e37b417d3a8fb88fc502603
knrtaven <52444839+knrtaven@users.noreply.github.com>
2025-07-04 23:16:12 +0800

    removed unusued module

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,7 +3,6 @@
 import NotificationDropdown from "@/components/header/NotificationDropdown";
 import UserDropdown from "@/components/header/UserDropdown";
 import { useSidebar } from "@/context/SidebarContext";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState} from "react";
 
