b192e10d28e466a47c2fb2b8439c3d82a1ea9bb1
xdjeremy <jeremy.fran@icloud.com>
2025-07-08 14:20:31 +0800

    Enhance ConsumptionOverviewChart component by adding additional descriptive text to improve user understanding of consumption metrics. Adjust layout for better visual organization and clarity.

--- src/components/insights/overview/ConsumptionOverviewChart.tsx
+++ src/components/insights/overview/ConsumptionOverviewChart.tsx
@@ -129,18 +129,38 @@ const ConsumptionOverviewChart = () => {
   ];
 
   return (
-    <div className="h-full w-full shrink-0 overflow-hidden rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
-      <div className="flex flex-col px-6 py-8">
-        {/* Title and description */}
-        <h2 className="text-lg font-medium dark:text-white">Consumption</h2>
-        <p className="text-sm font-light text-gray-500 dark:text-gray-400">Adjusted score</p>
-        <div className="mt-1 flex flex-row items-center gap-2 text-sm text-gray-600 dark:text-gray-400">
-          <Info className="h-4 w-4" />
-          <p>The degree to which leaders have completed the current months Lead-Rite content.</p>
-        </div>
+    <div className="flex flex-col gap-3">
+      <div className="h-full w-full shrink-0 overflow-hidden rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
+        <div className="flex flex-col px-6 py-8">
+          {/* Title and description */}
+          <h2 className="text-lg font-medium dark:text-white">Consumption</h2>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">Adjusted score</p>
+          <div className="mt-1 flex flex-row items-center gap-2 text-sm text-gray-600 dark:text-gray-400">
+            <Info className="h-4 w-4" />
+            <p>The degree to which leaders have completed the current months Lead-Rite content.</p>
+          </div>
 
-        {/* Chart */}
-        <ReactApexChart options={options} series={series} type="area" height={310} />
+          {/* Chart */}
+          <ReactApexChart options={options} series={series} type="area" height={310} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          Consumption rate may be zero in the first month due to the baseline survey (as
+          there&apos;s no learning modules during this period).
+        </p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          The graphs below show the average percentages by category, based on the number of
+          responses received. Hover over any of the data points to view the number of responses.
+        </p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          The <span className='font-bold'>adjusted score</span> is the score based on how many people responded verses how many people
+          could have responded.
+        </p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          The <span className='font-bold'>raw score</span> is the score based on the ratings provided by the number of people who
+          responded.
+        </p>
       </div>
     </div>
   );
