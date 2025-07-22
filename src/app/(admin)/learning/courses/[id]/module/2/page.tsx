8bd9f69709902ab104bd7294bacbb63a4e4e8c21
knrtaven <52444839+knrtaven@users.noreply.github.com>
2025-07-22 18:48:45 +0800

    routing fixes

--- src/app/(admin)/learning/courses/[id]/module/2/page.tsx
+++
@@ -1,8 +0,0 @@
-import FiveDaysToAHealthierYou from '@/components/modules/5DaysToAHealthierYou'
-import React from 'react'
-
-export default function Module2Demo() {
-  return (
-    <FiveDaysToAHealthierYou/>
-  )
-}
